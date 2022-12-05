37211809b374157d1edfa0071901f8b538b56c6f
Aniket <aniket.v.tarapurkar99@gmail.com>
2022-12-05 08:48:14 +0530

    Adding enable dark mode and light mode in about section of react component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm'
 function App() {
@@ -12,6 +13,7 @@ function App() {
 {/* <Navbar/> */}
 <div className='container my-3'>
 <TextForm heading="Enter the text to analyse below "/>
+{/* <About/> */}
 </div>
   </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,10 +13,30 @@ const handleLoClick =() =>{
 let newText = text.toLowerCase();
 setText(newText);
 }
-
+const handleclearClick =() =>{
+    // console.log("Upper case was clicked :" + text)
+    let newText = '';
+    setText(newText);
+    }
+    const handleLocLoClick =() =>{
+        // console.log("Upper case was clicked :" + text)
+        let newText = text.toLocaleLowerCase();
+        setText(newText);
+        }
+    
 const handleOnChange= (event) =>{
 // console.log("On Change")
 setText(event.target.value)
+}
+const handleCopy=()=>{
+ 
+    var text= document.getElementById('myBox')
+    text.select();
+    navigator.clipboard.writeText(text.value);
+}
+const handleExtraSpaces= () =>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
 }
 
 const [text, setText]= useState('Enter text here');
@@ -36,7 +56,11 @@ return (
 <button className='btn btn-primary mx-2'  onClick={handleUpClick}> Convert to Uppercase</button>
 
 <button className='btn btn-primary  mx-2'  onClick={handleLoClick}> Convert to Lowercase</button>
+<button className='btn btn-primary  mx-2'  onClick={handleclearClick}> Clear</button>
 
+<button className='btn btn-primary  mx-2'  onClick={handleLocLoClick}> Try it</button>
+<button className='btn btn-primary  mx-2'  onClick={handleCopy}> Copy Text</button>
+<button className='btn btn-primary  mx-2'  onClick={handleExtraSpaces}> Remove Extra Spaces</button>
 </div>
 
 <div className="container">
