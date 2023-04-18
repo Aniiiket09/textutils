2c65beb5b852e8a8e38fec444ccc97cbaf95ba39
Aniket <aniket.v.tarapurkar99@gmail.com>
2023-04-18 16:04:30 +0530

    adding alert component in our app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,77 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { Routes } from "react-router-dom";
 
-import './App.css';
-// import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm'
+import "./App.css";
+import About from "./components/About";
+import Alert from "./components/Alert";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 function App() {
-  
+  const [mode, setMode] = useState("light"); // whetherstarte of  dark mode is on or off
+  const [alert, setAlert] = useState(null); // this is for alert function
+
+  const showalert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#172444";
+      showalert("  Dark mode is enable", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showalert("  Light mode is enable", "success");
+    }
+  };
   return (
-    
-  <>
-{/* <Navbar title="TextUtils" about="About Us"/> */}
-<Navbar title="TextUtils" />
-{/* <Navbar/> */}
-<div className='container my-3'>
-<TextForm heading="Enter the text to analyse below "/>
-{/* <About/> */}
-</div>
-  </>
+    <>
+      {/* <Navbar title="TextUtils" about="About Us" /> */}
+      {/* <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} /> */}
+      {/* <Navbar/> */}
+      {/* <TextForm
+        heading="Enter the text to analyse below "
+        showalert={showalert}
+      /> */}
+      {/* <About /> */}
+      {/* <Alert alert={alert} /> */}
+      <div className="container my-3">
+        {/* style={{ backgroundColor: "#172444" }} */}
+        <Router>
+          <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+          <Alert alert={alert} />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  heading="Enter the text to analyse below "
+                  showalert={showalert}
+                />
+              }
+            />
+
+            <Route exact path="/about" element={<About mode={mode} />} />
+            {/* <Route exact path="/register" element={<Signup />} /> */}
+
+            {/* <Route path="*" element={<Page404 />} /> */}
+            {/* React Router v6 does not use exact attribute to match the exact routes. By default it matches the exact route. To match any value use "*" in the route path. */}
+          </Routes>
+        </Router>
+      </div>
+    </>
   );
-} 
+}
 
 export default App;
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,75 +1,149 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-
-const handleUpClick =() =>{
-// console.log("Upper case was clicked :" + text)
-let newText = text.toUpperCase();
-setText(newText);
-}
-
-const handleLoClick =() =>{
-// console.log("Upper case was clicked :" + text)
-let newText = text.toLowerCase();
-setText(newText);
-}
-const handleclearClick =() =>{
+  const handleUpClick = () => {
     // console.log("Upper case was clicked :" + text)
-    let newText = '';
+    let newText = text.toUpperCase();
     setText(newText);
-    }
-    const handleLocLoClick =() =>{
-        // console.log("Upper case was clicked :" + text)
-        let newText = text.toLocaleLowerCase();
-        setText(newText);
-        }
-    
-const handleOnChange= (event) =>{
-// console.log("On Change")
-setText(event.target.value)
-}
-const handleCopy=()=>{
- 
-    var text= document.getElementById('myBox')
+    props.showalert(" Converted to uppercase", "success");
+  };
+
+  const handleLoClick = () => {
+    // console.log("Upper case was clicked :" + text)
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showalert("  Converted to lowercase", "success");
+  };
+  const handleclearClick = () => {
+    // console.log("Upper case was clicked :" + text)
+    let newText = "";
+    setText(newText);
+    props.showalert("  Text Has Been Cleared", "success");
+  };
+  const handleLocLoClick = () => {
+    // console.log("Upper case was clicked :" + text)
+    let newText = text.toLocaleLowerCase();
+    setText(newText);
+    props.showalert("  Converted to Lowercase of captitalize Word", "success");
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On Change")
+    setText(event.target.value);
+  };
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
-}
-const handleExtraSpaces= () =>{
+    props.showalert("  Text Has Copied", "success");
+  };
+  const handleExtraSpaces = () => {
     let newText = text.split(/[ ]+/);
     setText(newText.join(" "));
-}
+    props.showalert("  Extra Spaces Removed", "success");
+  };
 
-const [text, setText]= useState('Enter text here');
-// text ="new state" // wrong way to change the state
-// setText="new state anything" // correct way to change the state
-return (
-<>
+  const [text, setText] = useState("");
+  // text ="new state" // wrong way to change the state
+  // setText="new state anything" // correct way to change the state
+  return (
+    <>
+      <div
+        className="container"
+        style={{
+          color: props.mode === "dark" ? "white" : "black",
+          // backgroundColor: props.mode === "dark " ? "red" : "white",
+        }}
+      >
+        <h1
+          style={
+            {
+              // backgroundColor: props.mode === "dark " ? "blue" : "white",
+            }
+          }
+        >
+          {props.heading}
+        </h1>
 
-<div className='container'>
-    <h1>{props.heading}</h1>
+        <div
+          className="mb-3"
+          style={{
+            color: props.mode === "dark" ? "black" : "white",
+          }}
+        >
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+            style={{
+              // backgroundColor: props.mode === "dark " ? "white" : "blue",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+          ></textarea>
+        </div>
 
-<div className="mb-3">
+        <button className="btn btn-primary mx-2 my-2 " onClick={handleUpClick}>
+          {" "}
+          Convert to Uppercase
+        </button>
 
-<textarea className="form-control"  value={text}  onChange={handleOnChange} id="myBox" rows="8"></textarea>
-</div>
+        <button className="btn btn-primary  mx-2 my-2" onClick={handleLoClick}>
+          {" "}
+          Convert to Lowercase
+        </button>
+        <button
+          className="btn btn-primary  mx-2 my-2"
+          onClick={handleclearClick}
+        >
+          {" "}
+          Clear
+        </button>
 
-<button className='btn btn-primary mx-2'  onClick={handleUpClick}> Convert to Uppercase</button>
+        <button
+          className="btn btn-primary  mx-2 my-2"
+          onClick={handleLocLoClick}
+        >
+          {" "}
+          Try it
+        </button>
+        <button className="btn btn-primary  mx-2 my-2" onClick={handleCopy}>
+          {" "}
+          Copy Text
+        </button>
+        <button
+          className="btn btn-primary  mx-2 my-2"
+          onClick={handleExtraSpaces}
+        >
+          {" "}
+          Remove Extra Spaces
+        </button>
+      </div>
 
-<button className='btn btn-primary  mx-2'  onClick={handleLoClick}> Convert to Lowercase</button>
-<button className='btn btn-primary  mx-2'  onClick={handleclearClick}> Clear</button>
-
-<button className='btn btn-primary  mx-2'  onClick={handleLocLoClick}> Try it</button>
-<button className='btn btn-primary  mx-2'  onClick={handleCopy}> Copy Text</button>
-<button className='btn btn-primary  mx-2'  onClick={handleExtraSpaces}> Remove Extra Spaces</button>
-</div>
-
-<div className="container">
-    <h2>Your Text Summary</h2>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{0.008 * text.split(" ").length } minutes to read words</p>
-    <h2>Preview</h2>
-    <p>{text}</p>
-</div>
-</>
-)
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "black" : "white" }}
+      >
+        <h2>Your Text Summary</h2>
+        <p>
+          {
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length
+          }{" "}
+          words and {text.length} characters
+        </p>
+        <p>
+          {0.008 *
+            text.split(" ").filter((element) => {
+              return element.length !== 0;
+            }).length}{" "}
+          minutes to read words
+        </p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Nothing To Preview!"}</p>
+      </div>
+    </>
+  );
 }
